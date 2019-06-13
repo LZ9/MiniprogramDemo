0f2e361399c9558166b9b0b2915e2fcafa18491a
LZ9 <lz.mms@foxmail.com>
2019-06-13 14:53:32 +0800

    添加列表样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,14 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    pokemons: [{
+      name: "妙蛙种子"
+    }, {
+      name: "小火龙"
+    }, {
+      name: "杰尼龟"
+    }]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,13 +22,13 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
